77ceec5f44a97f504d6f59aeb3540a57884ef89f
KnlnKS <soulkks@hotmail.ca>
2022-09-30 18:36:28 -0400

    🔧 Update settings

--- letterboxd/settings.py
+++ letterboxd/settings.py
@@ -18,6 +18,7 @@ NEWSPIDER_MODULE = 'letterboxd.spiders'
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = False
+FEED_EXPORT_ENCODING = 'utf-8'
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
